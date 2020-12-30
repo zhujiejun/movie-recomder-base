13d100164807ff1c74abd092ccefa4c6cea90e84
zhujiejun <2383322468@qq.com>
2020-12-30 11:52:25 +0800

    commit

--- service-000/src/main/scala/com/zhujiejun/recomder/cons/Const.scala
+++ service-000/src/main/scala/com/zhujiejun/recomder/cons/Const.scala
@@ -37,26 +37,26 @@ object Const {
     val RATING_DATA_PATH = "/home/cat/Downloads/common/ratings.csv"
     val TAG_DATA_PATH = "/home/cat/Downloads/common/tags.csv"
 
-    //000原始数据表的名称
+    //000原始数据表名称
     val ORIGINAL_MOVIE_TABLE_NAME = "sfb_original"
     val ORIGINAL_MOVIE_COLUMN_FAMILY = "original_movie"
     val ORIGINAL_RATING_COLUMN_FAMILY = "original_rating"
     val ORIGINAL_TAG_COLUMN_FAMILY = "original_tag"
 
-    //001统计的表的名称
+    //001统计表名称
     val STATIC_MOVIE_TABLE_NAME = "sfb_static"
     val RATE_MORE_MOVIES_COLUMN_FAMILY = "rate_more_movies"
     val RATE_MORE_RECENTLY_MOVIES_COLUMN_FAMILY = "rate_more_recently_movies"
     val AVERAGE_MOVIES_COLUMN_FAMILY = "average_movies"
     val GENRES_TOP_MOVIES_COLUMN_FAMILY = "genres_top_movies"
 
-    //002离线的表的名称
+    //002离线表名称
     val OFFLINE_MOVIE_TABLE_NAME = "sfb_offline"
     val USER_RECS_COLUMN_FAMILY = "user_recs"
     val MOVIE_FEATURES_RECS_COLUMN_FAMILY = "movie_features_recs"
     val MOVIE_CONTENTS_RECS_COLUMN_FAMILY = "movie_contents_recs"
 
-    //003实时的表的名称
+    //003实时表名称
     val STREAM_MOVIE_TABLE_NAME = "sfb_stream"
     val STREAM_RECS_COLUMN_FAMILY = "stream_recs"
 }
