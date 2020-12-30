75ca6dd8152aa56a46fb8e7b001599061b1b3b06
zhujiejun <2383322468@qq.com>
2020-12-30 15:03:56 +0800

    commit

--- service-000/src/main/scala/com/zhujiejun/recomder/data/RateMoreRecentlyMovie.scala
+++ service-000/src/main/scala/com/zhujiejun/recomder/data/RateMoreRecentlyMovie.scala
@@ -1,3 +1,12 @@
 package com.zhujiejun.recomder.data
 
+/**
+ * |-- mid: integer (nullable = false)
+ * |-- count: long (nullable = false)
+ * |-- yearmonth: integer (nullable = false)
+ *
+ * @param mid
+ * @param count
+ * @param yearmonth
+ */
 case class RateMoreRecentlyMovie(mid: Int, count: Long, yearmonth: Int)
